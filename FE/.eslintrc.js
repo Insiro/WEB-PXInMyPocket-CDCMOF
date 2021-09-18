e78d6cb14955c5d97cc9dd4670875abb344723fa
Insiro <leeon@insiro.me>
2021-09-18 14:12:32 +0000

    chore: update configurations upgrade yarn update eslint rule change gitignore add twilwind pacages

--- FE/.eslintrc.js
+++ FE/.eslintrc.js
@@ -29,7 +29,7 @@ module.exports = {
         MemberExpression: 0,
       },
     ],
-
+    "@typescript-eslint/no-inferrable-types":"off",
     "no-inferrable-types": "off",
     "no-await-in-loop": "error",
     "no-return-await": "error",
