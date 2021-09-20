edb23a467b947c2a277cbb22e815a51cfc23cee7
Insiro <leeon@insiro.me>
2021-09-20 10:56:43 +0000

    add: sidebar

---
+++ FE/src/components/Layout.vue
@@ -0,0 +1,13 @@
+<template>
+  <SideBar></SideBar>
+  <slot></slot>
+</template>
+<script lang="ts">
+import { SideBar } from "./SideBar";
+import { Vue, Options } from "vue-class-component";
+// import { Pages } from "@/router";
+@Options({
+  components: { SideBar },
+})
+export default class Layout extends Vue {}
+</script>
---
+++ FE/src/components/SideBar/SideBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <SideBarFrame :abv="abv" :title="title" :backgroundColor="backgroundColor">
+    <Item v-for="item in links" :key="item.name" :link="item"></Item>
+  </SideBarFrame>
+  <slot></slot>
+</template>
+<script lang="ts">
+import { Vue, Options, prop } from "vue-class-component";
+import SideBarFrame from "./SideBarFrame.vue";
+import Item from "./Item.vue";
+import { Pages } from "@/router";
+// import { Pages } from "@/router";
+
+class Props {
+  title = prop<string>({ default: "testTitle" });
+  backgroundColor = prop<string>({ default: "green" });
+  abv = prop<string>({ default: "abv" });
+}
+
+@Options({
+  components: { SideBarFrame, Item },
+})
+export default class SideBar extends Vue.with(Props) {
+  links: Array<Pages.pgobj> = Pages.list;
+}
+</script>
