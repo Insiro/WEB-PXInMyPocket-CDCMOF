8f1656a2d75a1805836f92014387c2a691dc955a
Insiro <leeon@insiro.me>
2021-09-22 14:03:31 +0000

    refactor : clean Router

--- FE/src/router/index.ts
+++ FE/src/router/index.ts
@@ -1,25 +1,43 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import Home from "../views/Home.vue";
-import * as Pages from "./pagelist";
 import About from "../views/About.vue";
-const routes: Array<RouteRecordRaw> = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home,
-  },
-  {
-    path: "/about",
-    name: "About",
-    component: About,
+import Components from "@/views/Components.vue";
+export interface pageObj {
+  icon?: string | null;
+  name: string;
+  url: string;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  component: any;
+}
 
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    // component: () =>
-    //   import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
+export const pageList: Array<pageObj> = [
+  { icon: null, name: "Home", url: "/", component: Home },
+  { icon: null, name: "About", url: "/about", component: About },
+  { icon: null, name: "Components", url: "/components", component: Components },
 ];
+
+export const routes: Array<RouteRecordRaw> = pageList.map((item) => {
+  return { path: item.url, name: item.name, component: item.component };
+});
+
+// const routes: Array<RouteRecordRaw> = [
+//   {
+//     path: "/",
+//     name: "Home",
+//     component: Home,
+//   },
+//   {
+//     path: "/about",
+//     name: "About",
+//     component: About,
+
+//     // route level code-splitting
+//     // this generates a separate chunk (about.[hash].js) for this route
+//     // which is lazy-loaded when the route is visited.
+//     // component: () =>
+//     //   import(/* webpackChunkName: "about" */ "../views/About.vue"),
+//   },
+// ];
 
 const router = createRouter({
   history: createWebHistory(),
@@ -28,4 +46,3 @@ const router = createRouter({
 });
 
 export default router;
-export { Pages };
