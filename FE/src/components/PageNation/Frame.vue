e388086aa061e4fe00686d39b82aef40b91311eb
Insiro <leeon@insiro.me>
2021-09-22 14:05:53 +0000

    add: Components ComponentFrame, PageNation

---
+++ FE/src/components/PageNation/Frame.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex mr-4 rounded">
+    <slot>
+      <Item text="item" />
+    </slot>
+  </div>
+</template>
+<script lang="ts">
+import { Vue, Options, prop } from "vue-class-component";
+import Item from "./Item.vue";
+@Options({ components: { Item } })
+export default class PgFrame extends Vue {}
+</script>
---
+++ FE/src/components/PageNation/Item.vue
@@ -0,0 +1,34 @@
+<template>
+  <a
+    :href="href"
+    class="
+      px-3
+      py-2
+      leading-tight
+      text-indigo-700
+      bg-white
+      border border-r-0 border-gray-200
+      hover:bg-indigo-500 hover:text-white
+    "
+    :class="[
+      position === 'left' ? leftend : position === 'right' ? rightend : middle,
+    ]"
+  >
+    <span>{{ text }}</span>
+  </a>
+</template>
+
+<script lang="ts">
+import { Options, Vue, prop } from "vue-class-component";
+import { ref } from "vue";
+class Props {
+  text = prop<string>({ default: "pagenation Item" });
+  position = prop<string>({ default: "middle" });
+  href = prop<string>({ default: "#" });
+}
+export default class PgItem extends Vue.with(Props) {
+  leftend = ref("ml-0 rounded-l border-r-0");
+  rightend = ref("rounded-r");
+  middle = ref("border-r-0");
+}
+</script>
