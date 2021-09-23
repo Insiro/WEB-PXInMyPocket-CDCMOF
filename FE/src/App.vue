d139d57a59cfb9a01c2d5ee6e3bb60a916478b54
Insiro <leeon@insiro.me>
2021-09-23 10:30:20 +0000

    add: dynamic component for empty pages

--- FE/src/App.vue
+++ FE/src/App.vue
@@ -1,9 +1,19 @@
 <template>
-  <baseLayout>
+  <component :is="layout">
     <router-view />
-  </baseLayout>
+  </component>
 </template>
 
-<style>
-</style>
-<script lang="ts"></script>
+<style></style>
+<script lang="ts">
+import { Vue, Options } from "vue-class-component";
+import { useRoute } from "vue-router";
+import { Layout, EmptyLayout } from "@/components/Layout";
+
+@Options({ components: { Layout, EmptyLayout } })
+export default class App extends Vue {
+  get layout() {
+    return useRoute().meta.noLayout === true ? "EmptyLayout" : "Layout";
+  }
+}
+</script>
---
+++ FE/src/components/Layout/Empty.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <slot />
+  </div>
+</template>
+<script lang="ts">
+import { Vue } from "vue-class-component";
+
+export default class EmptyLayout extends Vue {}
+</script>
