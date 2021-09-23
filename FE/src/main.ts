9766e55134634d126172907f9ec2978485a61576
Insiro <leeon@insiro.me>
2021-09-23 10:30:20 +0000

    add: dynamic component for empty pages

--- FE/src/main.ts
+++ FE/src/main.ts
@@ -7,11 +7,9 @@ import store from "./store";
 import "reset.css";
 import "normalize.css";
 import "./assets/main.css";
-import Layout from "@/components/Layout";
 
 const app = createApp(App);
 
-app.component("baseLayout", Layout);
 app.use(store);
 app.use(router);
 app.mount("#app");
