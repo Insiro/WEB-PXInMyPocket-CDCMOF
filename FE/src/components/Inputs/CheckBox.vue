0748b09489fb8d3121fd1db1d49c1597f5e4059e
Insiro <leeon@insiro.me>
2021-09-25 05:22:50 +0000

    add : Compoents - CheckBox, IconBtn, Btn fix : textinput event passing

--- FE/src/components/Inputs/CheckBox.vue
+++ FE/src/components/Inputs/CheckBox.vue
@@ -1,17 +1,28 @@
 <template>
   <label>
     <input
+      v-model="value"
       type="checkbox"
       class="w-5 h-5 text-indigo-600 rounded-md focus:ring-indigo-500"
       :name="name"
-    /><span class="ml-2 text-gray-700">{{ text }}</span>
+      @change="onInputChanged"
+    />
+    <span class="ml-2 text-gray-700">
+      <span class="ml-2 text-gray-700">
+        <slot>CheckBox</slot>
+      </span>
+    </span>
   </label>
 </template>
 <script lang="ts">
 import { Vue, prop } from "vue-class-component";
 class Prop {
-  text = prop<string>({ default: "checkbox" });
   name = prop<string>({ default: "checkbox" });
+  value = prop<string>({ default: "Checkbox" });
 }
-export default class Name extends Vue.with(Prop) {}
+export default class Name extends Vue.with(Prop) {
+  onInputChanged(e: Event): void {
+    this.$emit("updates", (e.target as HTMLInputElement).value);
+  }
+}
 </script>
