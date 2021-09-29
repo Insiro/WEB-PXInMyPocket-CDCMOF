5eb49ac08e116b4d18cb76fa1ca7b49b1d3cf2df
Insiro <leeon@insiro.me>
2021-09-29 10:50:40 +0000

    add: Table Components (default Table)

---
+++ FE/src/components/Table/Item/Item.vue
@@ -0,0 +1,14 @@
+<template>
+  <td :class="[styleClass]" :colspan="colspan"><slot /></td>
+</template>
+
+<script lang="ts">
+import { prop, Vue } from "vue-class-component";
+import { ref } from "vue";
+class Prop {
+  colspan = prop<number>({ default: 1 });
+}
+export default class Item extends Vue.with(Prop) {
+  styleClass = ref("px-6 py-4 border-b border-gray-200 whitespace-nowrap");
+}
+</script>
---
+++ FE/src/components/Table/Table/Table.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="py-2 -my-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
+    <div
+      class="
+        inline-block
+        min-w-full
+        overflow-hidden
+        align-middle
+        border-b border-gray-200
+        shadow
+        sm:rounded-lg
+      "
+    >
+      <table class="min-w-full">
+        <thead>
+          <tr>
+            <slot name="header">
+              <TableHead :clsas="[thClass]">Table</TableHead>
+              <TableHead>Header</TableHead>
+            </slot>
+          </tr>
+        </thead>
+
+        <tbody class="bg-white">
+          <slot name="body">
+            <tr>
+              <TableItem>Table</TableItem>
+              <TableItem>Item</TableItem>
+            </tr>
+          </slot>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { ref } from "vue";
+import { Options, Vue } from "vue-class-component";
+import TableHead from "../Head";
+import TableItem from "../Item";
+
+@Options({ components: { TableHead, TableItem } })
+export default class Table extends Vue {
+  thClass = ref(
+    "text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase"
+  );
+}
+</script>
