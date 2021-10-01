f256d5001a1c608ef76f1c026210ae323015b413
Insiro <leeon@insiro.me>
2021-10-01 08:52:42 +0000

    add: eslint config

---
+++ BE/backEndServer/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: "module",
+  },
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": "error",
+  },
+};
