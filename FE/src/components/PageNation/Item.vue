a4b5e3d64ba2a4a1c40abea91521814eba64ef90
Insiro <leeon@insiro.me>
2021-10-03 00:24:08 +0000

    add: pageNation moving function

--- FE/src/components/PageNation/Item.vue
+++ FE/src/components/PageNation/Item.vue
@@ -12,6 +12,7 @@
     "
     :class="[
       position === 'left' ? leftend : position === 'right' ? rightend : middle,
+      page_active === true ? activeClass : '',
     ]"
     @click="onItemClick"
   >
@@ -23,14 +24,16 @@
 import { Vue, prop } from "vue-class-component";
 import { ref } from "vue";
 class Props {
-  text = prop<string>({ default: "pagenation Item" });
+  text = prop<string>({ default: "0" });
   position = prop<string>({ default: "middle" });
   href = prop<string>({ default: "#" });
+  page_active = prop<boolean>({ default: false });
 }
 export default class PgItem extends Vue.with(Props) {
   leftend = ref("ml-0 rounded-l border-r-0");
   rightend = ref("rounded-r");
   middle = ref("border-r-0");
+  activeClass = ref("bg-indigo-500 text-white");
   onItemClick(): void {
     this.$emit("pageItemClick", this.text);
   }
