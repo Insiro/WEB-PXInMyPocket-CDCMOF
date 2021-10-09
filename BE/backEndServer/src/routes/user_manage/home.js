7a7eb091f59fd9822d43a9351bd40a428d5c307a
Insiro <leeon@insiro.me>
2021-10-09 06:57:22 +0000

    fix: lint be

--- BE/backEndServer/src/routes/user_manage/home.js
+++ BE/backEndServer/src/routes/user_manage/home.js
@@ -11,8 +11,6 @@ var router = express.Router();
 //not allowd api
 router.get("/", HttpError.badRequest);
 
-
-
 //FE에 세션에 대한 정보를 알려주기 위한 부분입니다.
 router.all("/signed", (req, res) => {
   res.status(200).json({ value: req.session.user.authorized === true });
