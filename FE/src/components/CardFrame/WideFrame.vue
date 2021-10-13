4b7a81169c9ed7ab22fd56ad37e7759c3542f4b8
Insiro <leeon@insiro.me>
2021-10-13 13:46:28 +0000

    refactor: wideFrame can access container

--- FE/src/components/CardFrame/WideFrame.vue
+++ FE/src/components/CardFrame/WideFrame.vue
@@ -1,11 +1,20 @@
 <template>
   <div class="mt-8">
-    <h4 v-if="title !== undefined" class="text-gray-600">{{ title }}</h4>
+    <h4
+      v-if="title !== undefined"
+      class="text-3xl font-medium text-gray-700 my-3"
+    >
+      {{ title }}
+    </h4>
 
     <div class="mt-4">
       <div
         class="flex px-4 py-4 overflow-x-auto rounded-md"
-        :class="[bg_color, items_center ? 'justify-center' : '']"
+        :class="[
+          bg_color,
+          container_class,
+          items_center ? 'justify-center' : '',
+        ]"
       >
         <slot />
       </div>
@@ -19,6 +28,7 @@ class Props {
   title?: string;
   bg_color = prop<string>({ default: "bg-white" });
   items_center = prop<boolean>({ default: false });
+  container_class = prop<string>({ default: "" });
 }
 export default class WideFrame extends Vue.with(Props) {}
 </script>
