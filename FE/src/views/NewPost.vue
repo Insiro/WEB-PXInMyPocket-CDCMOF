b639a63a277a5d4791d72a5192f48394abd04086
Insiro <leeon@insiro.me>
2021-10-17 06:50:20 +0000

    fix: newPost style

--- FE/src/views/NewPost.vue
+++ FE/src/views/NewPost.vue
@@ -1,46 +1,55 @@
 <template>
-  <div class="flex flex-col h-full w-full mx-5">
-    <div class="flex mt-3 postItem flex-row">
-      <TextInput
-        placeholder="제목"
-        class="flex-grow"
-        :value="title"
-        @updates="onTitleChanged"
-      />
-      <div v-show="is_admin" class="flex-grow-0">
-        <span class="inline-block">분류 : </span>
-        <select v-model="isNotic" class="ui dropdown inline-block">
-          <option :value="true">Notic</option>
-          <option :value="false">Normal</option>
-        </select>
+  <div class="flex flex-row h-full">
+    <div :class="BoxStyle" />
+    <div class="flex flex-col h-full postItem flex-grow">
+      <div class="flex mt-3 postItem flex-row">
+        <TextInput
+          placeholder="제목"
+          class="flex-grow"
+          :value="title"
+          @updates="onTitleChanged"
+        />
+        <div v-show="is_admin" class="flex-grow-0">
+          <span class="inline-block">분류 : </span>
+          <select v-model="isNotic" class="ui dropdown inline-block">
+            <option :value="true">Notic</option>
+            <option :value="false">Normal</option>
+          </select>
+        </div>
       </div>
-    </div>
 
-    <WideFrame class="flex-grow postItem">
-      <textarea
-        v-model="content"
-        class="h-full"
-        placeholder="게시글을 입력하세요"
-      ></textarea>
-    </WideFrame>
-    <Button class="flex-grow-0 mt-5" @Click="submitPost">게시</Button>
+      <WideFrame class="flex-grow postItem">
+        <textarea
+          v-model="content"
+          class="h-full w-full"
+          placeholder="게시글을 입력하세요"
+        ></textarea>
+      </WideFrame>
+      <Button class="flex-grow-0 mt-5 postItem" @Click="submitPost"
+        >게시</Button
+      >
+    </div>
+    <div :class="BoxStyle" />
   </div>
 </template>
 <script lang="ts">
+import axios from "axios";
+import { Options, Vue } from "vue-class-component";
+import { ref } from "vue";
+import { useToast } from "vue-toastification";
+import { apiUrl } from "@/utils";
+
 import { WideFrame } from "@/components/CardFrame";
 import { TextInput } from "@/components/Inputs";
-import { Options, Vue } from "vue-class-component";
-import userState from "@/store/User";
 import Button from "@/components/Button";
-import axios from "axios";
-import { apiUrl } from "@/utils";
-import { useToast } from "vue-toastification";
+import userState from "@/store/User";
 @Options({ components: { WideFrame, TextInput, Button } })
 export default class NewPost extends Vue {
   content = "";
   title = "";
   toast = useToast();
   isNotic = false;
+  BoxStyle = ref("flex-grow BOX");
   onTitleChanged(data: string): void {
     this.title = data;
   }
@@ -69,5 +78,13 @@ export default class NewPost extends Vue {
 <style scoped>
 .postItem {
   max-width: 750px;
+}
+@media (max-width: 768px) {
+  .BOX {
+    max-width: 10% !important;
+  }
+}
+.BOX {
+  max-width: 20%;
 }
 </style>
